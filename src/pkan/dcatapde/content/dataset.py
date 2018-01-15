ac24842ad20398530d6686a6887eb337e27930ca
Sandra Rum <sandra.rum@inqbus.de>
2018-01-15 18:08:24 +0100

    Corrected dataset and distribution interface to use new fields.

--- src/pkan/dcatapde/content/dataset.py
+++ src/pkan/dcatapde/content/dataset.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
+
 from pkan.dcatapde import _
 from pkan.dcatapde.content.foafagent import IFoafagent
 from pkan.dcatapde.content.literal import ILiteral
+from plone.autoform import directives as form
 from plone.dexterity.content import Container
+from plone.formwidget.relateditems import RelatedItemsFieldWidget
 from plone.supermodel import model
+from ps.zope.i18nfield.field import I18NText
+from ps.zope.i18nfield.field import I18NTextLine
 from z3c.form.object import registerFactoryAdapter
+from z3c.relationfield import RelationChoice
 from zope.interface import alsoProvides
 from zope.interface import implementer
 
@@ -15,22 +21,30 @@ class IDataset(model.Schema):
     """ Marker interfce and Dexterity Python Schema for Dataset
     """
 
-    add_title = schema.List(
-         title=_(u'Translated Title'),
-         required=False,
-         value_type=schema.Object(ILiteral),
+    add_title = I18NTextLine(
+        title=_(u'Translated Title'),
+        required=False,
     )
 
-    add_description = schema.List(
-         title=_(u'Translated Description'),
-         required=False,
-         value_type=schema.Object(ILiteral)
+    add_description = I18NText(
+        title=_(u'Translated Description'),
+        required=False,
     )
 
-    contributorID = schema.List(
-         title=_(u'Contributor'),
-         required=True,
-         value_type=schema.Object(IFoafagent)
+    form.widget(
+        'contributorID',
+        RelatedItemsFieldWidget,
+        content_type='foafagent',
+        content_type_title=_(u'Contributor'),
+        initial_path='/',
+        pattern_options={
+            'selectableTypes': ['foafagent'],
+        }
+    )
+    contributorID = RelationChoice(
+        title=_(u'Contributor'),
+        vocabulary='plone.app.vocabularies.Catalog',
+        required=True,
     )
 
     license = schema.URI(
