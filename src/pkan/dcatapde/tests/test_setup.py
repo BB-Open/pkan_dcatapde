81147301a2dbcb6c134b62ee9f9c24cae80ef1d5
Thomas Massmann <thomas.massmann@it-spir.it>
2018-01-25 12:53:29 +0100

    Fix code-analysis errors in test_setup.

--- src/pkan/dcatapde/tests/test_setup.py
+++ src/pkan/dcatapde/tests/test_setup.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 """Setup tests for this package."""
-from pkan.dcatapde.testing import PKAN_DCATAPDE_INTEGRATION_TESTING  # noqa
+
+from pkan.dcatapde.testing import PKAN_DCATAPDE_INTEGRATION_TESTING
 from plone import api
+from plone.browserlayer.utils import registered_layers
 
 import unittest
 
 
 class TestSetup(unittest.TestCase):
-    """Test that pkan.dcatapde is properly installed."""
+    """Validate setup process for pkan.dcatapde."""
 
     layer = PKAN_DCATAPDE_INTEGRATION_TESTING
 
@@ -17,39 +19,35 @@ class TestSetup(unittest.TestCase):
         self.installer = api.portal.get_tool('portal_quickinstaller')
 
     def test_product_installed(self):
-        """Test if pkan.dcatapde is installed."""
-        self.assertTrue(self.installer.isProductInstalled(
-            'pkan.dcatapde'))
+        """Validate that our product is installed."""
+        self.assertTrue(
+            self.installer.isProductInstalled('pkan.dcatapde'),
+        )
 
     def test_browserlayer(self):
-        """Test that IPkanDcatapdeLayer is registered."""
-        from pkan.dcatapde.interfaces import (
-            IPkanDcatapdeLayer)
-        from plone.browserlayer import utils
-        self.assertIn(
-            IPkanDcatapdeLayer,
-            utils.registered_layers())
+        """Validate that the browserlayer for our product is installed."""
+        layers = [l.getName() for l in registered_layers()]
+        self.assertIn('IPkanDcatapdeLayer', layers)
 
 
 class TestUninstall(unittest.TestCase):
+    """Validate uninstall process for pkan.dcatapde."""
 
     layer = PKAN_DCATAPDE_INTEGRATION_TESTING
 
     def setUp(self):
+        """Custom shared utility setup for tests."""
         self.portal = self.layer['portal']
         self.installer = api.portal.get_tool('portal_quickinstaller')
         self.installer.uninstallProducts(['pkan.dcatapde'])
 
     def test_product_uninstalled(self):
-        """Test if pkan.dcatapde is cleanly uninstalled."""
-        self.assertFalse(self.installer.isProductInstalled(
-            'pkan.dcatapde'))
+        """Validate that our product is uninstalled."""
+        self.assertFalse(
+            self.installer.isProductInstalled('pkan.dcatapde'),
+        )
 
     def test_browserlayer_removed(self):
-        """Test that IPkanDcatapdeLayer is removed."""
-        from pkan.dcatapde.interfaces import \
-            IPkanDcatapdeLayer
-        from plone.browserlayer import utils
-        self.assertNotIn(
-           IPkanDcatapdeLayer,
-           utils.registered_layers())
+        """Validate that the browserlayer is removed."""
+        layers = [l.getName() for l in registered_layers()]
+        self.assertNotIn('IPkanDcatapdeLayer', layers)
