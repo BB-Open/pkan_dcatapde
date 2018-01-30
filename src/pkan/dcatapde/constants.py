a7559dd5c2d16956bf6f5091f69eb18f16f202d1
Thomas Massmann <thomas.massmann@it-spir.it>
2018-01-30 12:52:50 +0100

    Add FOLDER_LICENSES for default license folder id.

--- src/pkan/dcatapde/constants.py
+++ src/pkan/dcatapde/constants.py
@@ -12,17 +12,23 @@ CT_Harvester = 'harvester'
 CT_HarvesterFieldConfig = 'harvester_field_config'
 
 
-HARVESTER_FOLDER_TITLE = 'Harvester Folder'
-HARVESTER_FOLDER_ID = 'harvester_folder'
-
-HARVESTER_FIELD_CONFIG_TITLE = 'Field Config'
-HARVESTER_FIELD_CONFIG_ID = 'field_config'
-
 DCAT_CTs = [
     CT_Catalog,
     CT_Dataset,
     CT_Distribution,
 ]
 
+
 # HTML filled with error information from harvesting dry_run
 ERROR_HTML_LINE = '<p>Found error {error} on field {field}</p>'
+
+
+#: Default folder for licenses.
+FOLDER_LICENSES = 'licenses'
+
+HARVESTER_FIELD_CONFIG_TITLE = 'Field Config'
+HARVESTER_FIELD_CONFIG_ID = 'field_config'
+
+
+HARVESTER_FOLDER_TITLE = 'Harvester Folder'
+HARVESTER_FOLDER_ID = 'harvester_folder'
