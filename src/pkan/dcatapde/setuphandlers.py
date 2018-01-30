4b0ea6a61ddd559ec3f13f01df19795758a83a6b
Thomas Massmann <thomas.massmann@it-spir.it>
2018-01-30 12:53:17 +0100

    Create required default content on installation.

--- src/pkan/dcatapde/setuphandlers.py
+++ src/pkan/dcatapde/setuphandlers.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
+"""Post install import steps for pkan.dcatapde."""
+
+from pkan.dcatapde import constants
+from plone import api
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
 
 
 @implementer(INonInstallable)
 class HiddenProfiles(object):
+    """Hidden GS profiles."""
 
     def getNonInstallableProfiles(self):
-        """Hide uninstall profile from site-creation and quickinstaller."""
+        """Do not show on Plone's list of installable profiles."""
         return [
             'pkan.dcatapde:uninstall',
         ]
@@ -16,8 +21,46 @@ class HiddenProfiles(object):
 def post_install(context):
     """Post install script"""
     # Do something at the end of the installation of this package.
+    add_default_folders(context)
 
 
 def uninstall(context):
     """Uninstall script"""
     # Do something at the end of the uninstallation of this package.
+
+
+def add_default_folders(context):
+    """Add default folders on first install."""
+    portal = _get_navigation_root(context)
+    add_licenses_folder(portal)
+
+
+def add_licenses_folder(portal):
+    """Add licenses folder."""
+    licenses = portal.get(constants.FOLDER_LICENSES)
+    if not licenses:
+        licenses = api.content.create(
+            container=portal,
+            type='Folder',
+            id=constants.FOLDER_LICENSES,
+            title=u'Licenses',
+        )
+        _publish(licenses)
+
+
+def _get_navigation_root(context):
+    """Find the correct navigation root."""
+    documents = api.content.find(portal_type='Document', id='front-page')
+    if len(documents) == 0:
+        return api.portal.get()
+    front_page = documents[0].getObject()
+
+    return api.portal.get_navigation_root(front_page)
+
+
+def _publish(content):
+    """Publish the object if it hasn't been published."""
+    if api.content.get_state(obj=content) != 'published':
+        api.content.transition(obj=content, transition='publish')
+        return True
+    return False
