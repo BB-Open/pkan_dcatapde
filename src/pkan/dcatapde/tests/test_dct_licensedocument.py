75d70408bd052c26f2e88154051a9f166c053896
Thomas Massmann <thomas.massmann@it-spir.it>
2018-01-30 14:50:29 +0100

    Adjust tests for dct_licensedocument content type.
    Adding a lincense somewehere else than the licenses folder using the API
    will fail because of the content rule.

--- src/pkan/dcatapde/tests/test_dct_licensedocument.py
+++ src/pkan/dcatapde/tests/test_dct_licensedocument.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """Content type tests for `dct_licensedocument`."""
 
+from pkan.dcatapde import constants
 from pkan.dcatapde import testing
-from pkan.dcatapde.constants import CT_DCT_LICENSE_DOCUMENT
 from pkan.dcatapde.content.dct_licensedocument import IDCTLicenseDocument
 from plone import api
 from plone.app.testing import setRoles
@@ -25,16 +25,25 @@ class DCTLicenseDocumentIntegrationTest(unittest.TestCase):
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
 
     def test_schema(self):
-        fti = queryUtility(IDexterityFTI, name=CT_DCT_LICENSE_DOCUMENT)
+        fti = queryUtility(
+            IDexterityFTI,
+            name=constants.CT_DCT_LICENSE_DOCUMENT,
+        )
         schema = fti.lookupSchema()
         self.assertEqual(IDCTLicenseDocument, schema)
 
     def test_fti(self):
-        fti = queryUtility(IDexterityFTI, name=CT_DCT_LICENSE_DOCUMENT)
+        fti = queryUtility(
+            IDexterityFTI,
+            name=constants.CT_DCT_LICENSE_DOCUMENT,
+        )
         self.assertTrue(fti)
 
     def test_factory(self):
-        fti = queryUtility(IDexterityFTI, name=CT_DCT_LICENSE_DOCUMENT)
+        fti = queryUtility(
+            IDexterityFTI,
+            name=constants.CT_DCT_LICENSE_DOCUMENT,
+        )
         factory = fti.factory
         obj = createObject(factory)
         self.assertTrue(IDCTLicenseDocument.providedBy(obj))
@@ -42,8 +51,8 @@ class DCTLicenseDocumentIntegrationTest(unittest.TestCase):
     def test_adding(self):
         setRoles(self.portal, TEST_USER_ID, ['Contributor'])
         obj = api.content.create(
-            container=self.portal,
-            type=CT_DCT_LICENSE_DOCUMENT,
+            container=self.portal.get(constants.FOLDER_LICENSES),
+            type=constants.CT_DCT_LICENSE_DOCUMENT,
             id='sample-license',
         )
         self.assertTrue(IDCTLicenseDocument.providedBy(obj))
