615dabe517072bff0576902c04d88f8d8eb53543
sandra <sandra.rum@inqbus.de>
2018-02-07 17:35:38 +0100

    Added Display-View for Field-Config

--- src/pkan/dcatapde/browser/content_views/harvester.py
+++ src/pkan/dcatapde/browser/content_views/harvester.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
+from pkan.dcatapde import utils
+from pkan.dcatapde.api.functions import get_ancestor
 from pkan.dcatapde.api.harvester import get_all_harvester
 from pkan.dcatapde.api.harvester_field_config import get_field_config
+from pkan.dcatapde.constants import CT_HARVESTER
+from pkan.dcatapde.content.harvester_field_config import CT_FIELD_RELATION
+from pkan.dcatapde.vocabularies.dcat_field import DcatFieldVocabulary
 from Products.Five import BrowserView
 
 
@@ -56,3 +61,46 @@ class ResetFieldsView(BrowserView):
         self.log = '<p>Reading fields done</p>'
 
         return super(ResetFieldsView, self).__call__(*args, **kwargs)
+
+
+class FieldConfigView(BrowserView):
+
+    def __call__(self, *args, **kwargs):
+        self.base_object_url = None
+        self.base_object_title = None
+
+        if self.context.base_object:
+            # fix: check why sometime to_object and sometimes not
+            base_object = getattr(self.context.base_object,
+                                  'to_object',
+                                  self.context.base_object)
+            self.base_object_title = base_object.Title()
+            self.base_object_url = base_object.absolute_url()
+
+        self.data = []
+
+        harvester = get_ancestor(self.context, CT_HARVESTER)
+
+        harvesting_type = harvester.harvesting_type(harvester)
+
+        used_cts = harvesting_type.get_used_cts()
+
+        for ct in used_cts:
+            if ct in CT_FIELD_RELATION:
+                field = CT_FIELD_RELATION[ct]
+                lines = getattr(self.context, field, [])
+                utils.set_request_annotations('pkan.vocabularies.context',
+                                              self.context)
+
+                vocab = DcatFieldVocabulary(ct)
+                options = vocab(self.context)
+
+                for line in lines:
+                    option = options.by_value[line['dcat_field']]
+                    line['dcat_field'] = option.title
+                self.data.append({
+                    'ct': ct,
+                    'lines': lines,
+                })
+
+        return super(FieldConfigView, self).__call__(*args, **kwargs)
