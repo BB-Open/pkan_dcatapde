d94c63a4451296a98fa04dad1d33709aadd22728
sandra <sandra.rum@inqbus.de>
2018-02-07 14:42:26 +0100

    Added HavesterOverview with all important links as DefaultView for HarvesterFolder. BugFixes on DataManager for empty Field Config

--- src/pkan/dcatapde/browser/content_views/harvester.py
+++ src/pkan/dcatapde/browser/content_views/harvester.py
@@ -1,5 +1,30 @@
 # -*- coding: utf-8 -*-
+from pkan.dcatapde.api.harvester import get_all_harvester
+from pkan.dcatapde.api.harvester_field_config import get_field_config
 from Products.Five import BrowserView
+
+
+class HarvesterOverview(BrowserView):
+
+    def __call__(self, *args, **kwargs):
+        harvester = get_all_harvester()
+
+        self.data = []
+
+        for harv in harvester:
+            path = harv.absolute_url()
+            field_config = get_field_config(harv)
+            self.data.append({
+                'title': harv.title,
+                'path': path,
+                'source_url': harv.url,
+                'dry_run': path + '/dry_run',
+                'real_run': path + '/real_run',
+                'field_config': field_config.absolute_url(),
+                'reset_fields': path + '/reset_fields',
+            })
+
+        return super(HarvesterOverview, self).__call__(*args, **kwargs)
 
 
 class DryRunView(BrowserView):
