9fe1569d89df40df384898589eb40326a98d8521
Thomas Massmann <thomas.massmann@it-spir.it>
2018-02-08 12:04:33 +0100

    Use Choice fields for reference in DCATDataset items.
    Switch to AjaxSelectAddFieldWidget.

--- src/pkan/dcatapde/content/dcat_dataset.py
+++ src/pkan/dcatapde/content/dcat_dataset.py
@@ -4,7 +4,7 @@
 from pkan.dcatapde import constants
 from pkan.dcatapde import i18n
 from pkan.dcatapde.content.base import DCATMixin
-from pkan.widgets.relateditems import RelatedItemsFieldWidget
+from pkan.widgets.ajaxselect import AjaxSelectAddFieldWidget
 from plone.autoform import directives as form
 from plone.dexterity.content import Container
 from plone.dexterity.factory import DexterityFactory
@@ -12,7 +12,6 @@ from plone.supermodel import model
 from ps.zope.i18nfield.field import I18NText
 from ps.zope.i18nfield.field import I18NTextLine
 from ps.zope.i18nfield.fieldproperty import I18NTextProperty
-from z3c.relationfield import RelationChoice
 from zope.interface import implementer
 
 import zope.schema as schema
@@ -77,89 +76,74 @@ class IDCATDataset(model.Schema):
 
     form.widget(
         'dct_publisher',
-        RelatedItemsFieldWidget,
+        AjaxSelectAddFieldWidget,
         content_type=constants.CT_FOAF_AGENT,
         content_type_title=i18n.LABEL_DCT_PUBLISHER,
         initial_path='/publisher/',
-        pattern_options={
-            'selectableTypes': [constants.CT_FOAF_AGENT],
-        },
     )
-    dct_publisher = RelationChoice(
+    dct_publisher = schema.Choice(
         description=i18n.HELP_DCT_PUBLISHER,
         required=True,
         title=i18n.LABEL_DCT_PUBLISHER,
-        vocabulary='plone.app.vocabularies.Catalog',
+        vocabulary='pkan.dcatapde.vocabularies.FOAFAgent',
     )
 
     # Recommended
     # -------------------------------------------------------------------------
     form.widget(
         'dct_creator',
-        RelatedItemsFieldWidget,
+        AjaxSelectAddFieldWidget,
         content_type=constants.CT_FOAF_AGENT,
         content_type_title=i18n.LABEL_DCT_CREATOR,
         initial_path='/agents/',
-        pattern_options={
-            'selectableTypes': [constants.CT_FOAF_AGENT],
-        },
     )
-    dct_creator = RelationChoice(
+    dct_creator = schema.Choice(
         description=i18n.HELP_DCT_CREATOR,
         required=False,
         title=i18n.LABEL_DCT_CREATOR,
-        vocabulary='plone.app.vocabularies.Catalog',
+        vocabulary='pkan.dcatapde.vocabularies.FOAFAgent',
     )
 
     form.widget(
         'dct_contributor',
-        RelatedItemsFieldWidget,
+        AjaxSelectAddFieldWidget,
         content_type=constants.CT_FOAF_AGENT,
         content_type_title=i18n.LABEL_DCT_CONTRIBUTOR,
         initial_path='/agents/',
-        pattern_options={
-            'selectableTypes': [constants.CT_FOAF_AGENT],
-        },
     )
-    dct_contributor = RelationChoice(
+    dct_contributor = schema.Choice(
         description=i18n.HELP_DCT_CONTRIBUTOR,
         required=False,
         title=i18n.LABEL_DCT_CONTRIBUTOR,
-        vocabulary='plone.app.vocabularies.Catalog',
+        vocabulary='pkan.dcatapde.vocabularies.FOAFAgent',
     )
 
     form.widget(
         'dcatde_originator',
-        RelatedItemsFieldWidget,
+        AjaxSelectAddFieldWidget,
         content_type=constants.CT_FOAF_AGENT,
         content_type_title=i18n.LABEL_DCATDE_ORIGINATOR,
         initial_path='/agents/',
-        pattern_options={
-            'selectableTypes': [constants.CT_FOAF_AGENT],
-        },
     )
-    dcatde_originator = RelationChoice(
+    dcatde_originator = schema.Choice(
         description=i18n.HELP_DACTDE_ORIGINATOR,
         required=False,
         title=i18n.LABEL_DCATDE_ORIGINATOR,
-        vocabulary='plone.app.vocabularies.Catalog',
+        vocabulary='pkan.dcatapde.vocabularies.FOAFAgent',
     )
 
     form.widget(
         'dcatde_maintainer',
-        RelatedItemsFieldWidget,
+        AjaxSelectAddFieldWidget,
         content_type=constants.CT_FOAF_AGENT,
         content_type_title=i18n.LABEL_DCATDE_MAINTAINER,
         initial_path='/agents/',
-        pattern_options={
-            'selectableTypes': [constants.CT_FOAF_AGENT],
-        },
     )
-    dcatde_maintainer = RelationChoice(
+    dcatde_maintainer = schema.Choice(
         description=i18n.HELP_DACTDE_MAINTAINER,
         required=False,
         title=i18n.LABEL_DCATDE_MAINTAINER,
-        vocabulary='plone.app.vocabularies.Catalog',
+        vocabulary='pkan.dcatapde.vocabularies.FOAFAgent',
     )
 
     dct_issued = schema.Date(
