86b6fc81bddbaf1ec333a95c091b5e23276b4ad4
Thomas Massmann <thomas.massmann@it-spir.it>
2018-03-07 19:09:47 +0100

    Fix custom edit form for Harvester CT.

--- src/pkan/dcatapde/browser/edit_views/harvester.py
+++ src/pkan/dcatapde/browser/edit_views/harvester.py
@@ -4,10 +4,13 @@ from pkan.dcatapde import utils
 from pkan.dcatapde.browser.harvester_entity.preview import PreviewFormMixin
 from pkan.dcatapde.content.harvester import IHarvester
 from plone.dexterity.browser import edit
+from plone.dexterity.interfaces import IDexterityEditForm
+from plone.z3cform import layout
 from z3c.form import button
+from zope.interface import classImplements
 
 
-class HarvesterEdit(edit.DefaultEditForm, PreviewFormMixin):
+class HarvesterEditForm(edit.DefaultEditForm, PreviewFormMixin):
 
     schema = IHarvester
     query_attr = 'top_node_sparql'
@@ -15,17 +18,21 @@ class HarvesterEdit(edit.DefaultEditForm, PreviewFormMixin):
     type_attr = 'source_type'
 
     def __init__(self, context, request, ti=None):
-        super(HarvesterEdit, self).__init__(context, request)
+        super(HarvesterEditForm, self).__init__(context, request)
         utils.set_request_annotations('pkan.vocabularies.context', context)
 
     @button.buttonAndHandler(_(u'Save'), name='save')
     def handleApply(self, action):
-        super(HarvesterEdit, self).handleApply(self, action)
+        super(HarvesterEditForm, self).handleApply(self, action)
 
     @button.buttonAndHandler(_(u'Cancel'), name='cancel')
     def handleCancel(self, action):
-        super(HarvesterEdit, self).handleCancel(self, action)
+        super(HarvesterEditForm, self).handleCancel(self, action)
 
     @button.buttonAndHandler(_(u'Run'))
     def handle_run(self, action):
         self.handle_preview(ignore_context=True)
+
+
+HarvesterEditView = layout.wrap_form(HarvesterEditForm)
+classImplements(HarvesterEditView, IDexterityEditForm)
