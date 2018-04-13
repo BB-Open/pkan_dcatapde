4ea95117e1dc1d10cc972f9660b96a16f37f1e37
sandra <sandra.rum@inqbus.de>
2018-04-13 17:25:04 +0200

    Corrected default View

--- src/pkan/dcatapde/browser/content_views/default_view.py
+++ src/pkan/dcatapde/browser/content_views/default_view.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 from plone.dexterity.browser.view import DefaultView
+from Products.CMFCore.interfaces import IFolderish
 
 
 class PKANDefaultView(DefaultView):
     """
     View for all Pkan-Types.
     """
+
+    def display_folder_listing(self):
+        return IFolderish.providedBy(self.context)
