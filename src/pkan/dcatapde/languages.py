979de78d005e5aa2f0d6a52ed8d87d26a64349dc
sandra <sandra.rum@inqbus.de>
2018-04-18 13:21:04 +0200

    Added custom LanguageAdapter.

---
+++ src/pkan/dcatapde/languages.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from pkan.dcatapde import _
+
+
+DEFAULT_LANGUAGE = u'gbr'
+
+AVAILABLE_LANGUAGES_ISO = {
+    u'de': u'deu',
+    u'us': u'usa',
+    u'gb': u'gbr',
+    u'uk': u'gbr',
+}
+
+AVAILABLE_LANGUAGES_TITLE = {
+    u'deu': _(u'German'),
+    u'usa': _(u'English (USA)'),
+    u'gbr': _(u'Egnlish (GB)'),
+}
