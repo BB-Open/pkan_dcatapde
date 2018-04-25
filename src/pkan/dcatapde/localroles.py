610fdf94e1b54b70911762b3659b95419238247b
sandra <sandra.rum@inqbus.de>
2018-04-25 12:48:27 +0200

    Added Roles to sharing Tab.

---
+++ src/pkan/dcatapde/localroles.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from pkan.dcatapde import _
+from plone.app.workflow import permissions
+from plone.app.workflow.interfaces import ISharingPageRole
+from plone.app.workflow.permissions import DelegateRoles
+from zope.interface import implementer
+
+
+@implementer(ISharingPageRole)
+class PkanEditor(object):
+
+    title = _(u'PkanEditor', default=u'PkanEditor')
+    required_permission = DelegateRoles
+    required_interface = None
+
+
+@implementer(ISharingPageRole)
+class CatalogAdmin(object):
+
+    title = _(u'CatalogAdmin', default=u'CatalogAdmin')
+    required_permission = permissions.DelegateEditorRole
+    required_interface = None
